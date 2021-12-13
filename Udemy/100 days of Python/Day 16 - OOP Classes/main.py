598137f13f2bae40d0e4292ef68afd690dd259cc
snapjak <jeremyabikhalil@gmail.com>
2021-12-13 16:59:26 +0400

    final

--- Udemy/100 days of Python/Day 16 - OOP Classes/main.py
+++ Udemy/100 days of Python/Day 16 - OOP Classes/main.py
@@ -2,97 +2,20 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
+is_on = True
 menu = Menu()
 coffee_maker = CoffeeMaker()
-money = 0
+money_machine = MoneyMachine()
 
-turn_off = False
-
-def check_user_input():
-    success = True
-    if choice == "off":
-        exit
-        turn_off = True
-    elif choice == "report":
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What do you want? {options}: ")
+    if choice == "report":
         coffee_maker.report()
-        print (f"Money: ${money}")
-        return success
-
-while turn_off == False:
-    choice = input(f"What would you like? {menu.get_items()}: ")
-
-check_user_input()
-    success = check_user_input()
-
-drink = menu.find_drink(choice)
-choice_cost = drink.cost
-
-
-#         print("Please insert valid choice.")
-#         success = False
-
-#     return success
-        
-
-# #Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
-# while turn_off == False:
-#     choice = input("What would you like? (espresso/latte/cappuccino): ")
-
-#     #Check the user’s input to decide what to do next.
-
-#     success = check_user_input()
-#     if not success:
-#         continue
-
-#     proceedToPayment = True
-#     # Check if resources sufficient
-#     for key in b.MENU:
-#         if key != choice:
-#             continue
-#         else:
-#             for key2 in resources:
-#                 if key2 not in b.MENU[choice]["ingredients"]:
-#                     continue
-#                 ingredients = (b.MENU[choice]["ingredients"])
-#                 if resources[key2] - ingredients[key2] <= 0:
-#                     print(f"Sorry there is not enough {key2}")
-#                     proceedToPayment = False
-#                     continue
-#                 else:
-#                     resources[key2] -= ingredients[key2]      
-#         cost = b.MENU[choice]["cost"]
-
-#     if not proceedToPayment:
-#         continue
-    
-#     # The prompt should show every time action has completed, e.g. once the drink is
-#     # dispensed. The prompt should show again to serve the next customer.
-
-#     # Process coins
-
-#     coins = {
-#         "quarters" : 0.25,
-#         "dimes" : 0.1,
-#         "nickles" : 0.05,
-#         "pennies" : 0.01,
-#         }
-
-#     print("Please insert coins.")
-#     def process_coins(coins):
-#         money = int(input(f"how many {coins}: "))
-#         return money
-
-#     for key in coins:
-#         total_paid += process_coins(key)*coins[key]
-
-    
-#     if total_paid < cost:
-#         total_paid = 0
-#         print("Sorry that's not enough money. Money refunded.")
-#     elif total_paid > cost:
-#         change = round((total_paid - cost),2)
-#         money += cost
-#         print(f"Here is ${change} in change.")
-
-#     print(f"Here is you {choice}. Enjoy!")
+        money_machine.report()
+    elif choice == "off":
+        is_on = False
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
