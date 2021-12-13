513eb11d95e72f50f5f2606c727692f6c9c70e5d
snapjak <jeremyabikhalil@gmail.com>
2021-12-13 21:36:00 +0400

    Quiz game

---
+++ Udemy/100 days of Python/Day 17 -  Creating Classes (Quiz game)/Test.py
@@ -0,0 +1,22 @@
+# Creating class, attributes and methods
+class User:
+    def __init__(self, user_id, user_name):
+        print("new user being created...")
+        self.id = user_id
+        self.user_name = user_name
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+user_1 = User("001", "Jeremy")
+user_2 = User("002", "Angela")
+print(user_1.user_name)
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.following)
+print(user_2.followers)
