5bc64d855ee06dc065cdcf095729812716dd79b2
snapjak <jeremyabikhalil@gmail.com>
2021-12-15 22:33:16 +0400

    hirst painting :)

--- Udemy/100 days of Python/Day 18 - Turtle and GUI/Painting.py
+++ Udemy/100 days of Python/Day 18 - Turtle and GUI/Painting.py
@@ -1,4 +1,14 @@
 import colorgram
 
-colors = colorgram.extract('hirst_painting.jpg', 10)
-print (colors)  
+colors = colorgram.extract('/home/jeremy/repos/Udemy/100 days of Python/Day 18 - Turtle and GUI/hirst_painting.jpg', 30)
+
+rgb_list = []
+rgb_tuple = ()
+
+for color in range(30):
+    first_color = colors[color]
+    rgb = first_color.rgb
+    rgb_tuple = (rgb[0],rgb[1],rgb[2])
+    rgb_list.append(rgb_tuple)
+
+print(rgb_list)
--- Udemy/100 days of Python/Day 18 - Turtle and GUI/main.py
+++ Udemy/100 days of Python/Day 18 - Turtle and GUI/main.py
@@ -1,4 +1,4 @@
-from turtle import Screen, Turtle
+from turtle import Screen, Turtle, setposition, st
 import random
 import turtle
 
@@ -6,7 +6,7 @@ tim = Turtle()
 tim.shape("turtle")
 tim.color("red")
 
-
+colors = [(40, 98, 148), (178, 46, 78), (205, 160, 93), (224, 210, 102), (137, 89, 63), (178, 165, 36), (110, 175, 208), (213, 130, 174), (228, 72, 48), (201, 74, 117), (92, 103, 188), (22, 154, 87), (122, 217, 208), (126, 41, 70), (95, 158, 62), (45, 190, 204), (226, 171, 187), (130, 189, 161), (213, 206, 38), (232, 171, 163), (172, 186, 222), (153, 208, 218), (104, 50, 38), (45, 62, 101), (44, 75, 80), (51, 61, 71)]
 # Draw a square
 # for x in range(0,4):
 #     tim.forward(100)
@@ -39,14 +39,14 @@ tim.color("red")
 #     tim.setheading(random.choice(directions))
 
 # # Create random color for turtle
-turtle.colormode(255)
+# turtle.colormode(255)
 
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    random_color = (r,g,b)
-    return random_color
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     random_color = (r,g,b)
+#     return random_color
 
 # directions = [0, 90 , 180, 270]
 
@@ -59,34 +59,59 @@ def random_color():
 
 # Drawing spirograph
 
+# tim.speed("fastest")
+# def draw_spirograph(gap_size):
+#     for _ in range(int(360/gap_size)):
+#         tim.color(random_color())
+#         tim.circle(100)
+#         current_heading = tim.heading()
+#         tim.setheading(current_heading +gap_size)
+
+
+# draw_spirograph(5)
+
+turtle.colormode(255)
 tim.speed("fastest")
-def draw_spirograph(gap_size):
-    for _ in range(int(360/gap_size)):
-        tim.color(random_color())
-        tim.circle(100)
-        current_heading = tim.heading()
-        tim.setheading(current_heading +gap_size)
+def hirst_painting():
+    for x in range(10):
+        tim.forward(10)
+        tim.penup()
+        tim.forward(10)
+        tim.pendown()
 
+def starting_position():
+    tim.hideturtle()
+    tim.penup()
+    tim.setposition(-450, -450)
 
-draw_spirograph(5)
+def draw_random_circle():
+    tim.pendown()
+    chosen_color = random.choice(colors)
+    tim.color(chosen_color)
+    tim.pencolor(chosen_color)
+    tim.fillcolor(chosen_color)
+    tim.begin_fill()
+    tim.circle(20)
+    tim.end_fill()
 
+def move_forward_invis():
+    tim.penup()
+    tim.hideturtle()
+    tim.forward(100)
 
+def reset_position(y):
+    tim.setposition(-450, -450 + y)
 
+starting_position()
+y = 100
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for f in range (10):
+    for x in range (10):
+        draw_random_circle()
+        move_forward_invis()
+    reset_position(y)
+    y += 100
 
 screen = Screen()
+screen.screensize(750,750)
 screen.exitonclick()
