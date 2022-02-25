653f7e9e48613d4b9003bb5549f37ac9a2ec753e
snapjak <jeremyabikhalil@gmail.com>
2022-02-25 19:59:42 +0400

    Day 29 done

--- Udemy/100 days of Python/Day 29 - Tkinter password manager/password-manager-start/main.py
+++ Udemy/100 days of Python/Day 29 - Tkinter password manager/password-manager-start/main.py
@@ -6,13 +6,37 @@ from email import message
 from statistics import mode
 from tkinter import *
 from venv import create
+from click import command
 from matplotlib import image
 from tkinter import messagebox
 from matplotlib.pyplot import text
+import random
+import pyperclip
 
+from scipy import rand
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for letter in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for sym in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for num in range(nr_numbers)]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -62,7 +86,7 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=16)
 password_entry.grid(row=3, column=1)
 
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save_data)
