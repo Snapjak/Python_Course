15fcabd37b47cda20fa2af4bb9d56b5bfc7b0e43
snapjak <jeremyabikhalil@gmail.com>
2022-04-09 14:46:21 +0400

    exce[topn handling

--- Udemy/100 days of Python/Day 30  - Errors and exceptions Json/main.py
+++ Udemy/100 days of Python/Day 30  - Errors and exceptions Json/main.py
@@ -1,8 +1,25 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
 
-from turtle import mode
+import pandas
 
+data = pandas.read_csv("/home/jeremy/repos/Udemy/100 days of Python/Day 30  - Errors and exceptions Json/nato_phonetic_alphabet.csv")
+#TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
-try:
-    file =  open("a_file.txt")
-except:
-    open(file="a_file.txt", mode="w")
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+is_false = True
+
+while is_false:
+    word = input("Enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        is_false = True
+        print("Sorry, only letters in the alphabet please")
+    else:
+        is_false = False
+        print(output_list)
